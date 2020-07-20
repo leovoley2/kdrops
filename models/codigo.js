9b92dc11edfbe6c331b1b37992cb3dee5996f534
leobardo sanabria <34109009+leovoley2@users.noreply.github.com>
2020-07-19 19:30:42 -0500

    busqueda8

--- models/codigo.js
+++ models/codigo.js
@@ -9,7 +9,7 @@ const Productos = db.define('producto', {
         autoIncrement : true
     }, 
     codigo: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(8),
         allowNull: false,
         validate : {
             notEmpty : {
